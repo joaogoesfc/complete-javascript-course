af3ccc8bc1db72e82744a947beeab8b40fd4147b
João Góes <goes.joaoseje@gmail.com>
2024-02-28 16:59:53 -0300

    Exercise 005: Operator precedence

---
+++ exercises/01-Fundamentals-Part-1/ex005-operator-precedence/script.js
@@ -0,0 +1,15 @@
+
+const now = 2037;
+const ageJoao = now - 2001;
+const ageSarah = now - 2020;
+
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJoao + ageSarah) / 2
+console.log(ageJoao, ageSarah, averageAge)
