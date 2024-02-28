108ae69bd92bb7d8152c4b624e8cc00cc4c53c0f
João Góes <goes.joaoseje@gmail.com>
2024-02-28 15:42:46 -0300

    Exercise 002: Data types

--- challenges/01-Fundamentals-Part-1/challenge000-values-and-variables/script.js
+++ challenges/01-Fundamentals-Part-1/challenge000-values-and-variables/script.js
@@ -1,18 +1,7 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+let country = "Brazil";
+let continent = "South America";
+let population = 200;
 
-console.log("Jonas");
-console.log(23)
-
-let firstName = "Matilda";
-
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-
-let jonas_matilda = "JM";
-let $function = 26; // allowed $, _ and letters at the beginning of the variables
-
-let PI = 3.1415; // constant
-
-let myFirstJob = "Programmer"; // better than job1
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ exercises/01-Fundamentals-Part-1/ex002-data-types/script.js
@@ -0,0 +1,20 @@
+
+let javascriptIsFun = true;
+console.log(true)
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(typeof year);
+
+year = 2024;
+console.log(typeof year);
+
+console.log(typeof null);
