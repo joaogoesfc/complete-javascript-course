2fe6ab78beb7642714d9d1f7de5e2234f744057b
João Góes <goes.joaoseje@gmail.com>
2024-02-28 16:04:51 -0300

    Exercise 003: Const and var

---
+++ exercises/01-Fundamentals-Part-1/ex003-const-and-var/script.js
@@ -0,0 +1,15 @@
+
+let age = 30;
+age = 31;
+
+const birthYear = 2000;
+
+//birthYear = 1992
+
+//const job;
+
+var job = 'programmer';
+job = 'developer'
+
+lastName = 'Góes' // avoid doing it
+console.log(lastName)
