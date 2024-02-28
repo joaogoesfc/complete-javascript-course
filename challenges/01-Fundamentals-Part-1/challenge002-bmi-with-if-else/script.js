b66c63fd472c3e0354b82e37deaca75202eeff93
João Góes <goes.joaoseje@gmail.com>
2024-02-28 19:09:15 -0300

    Challege 002: BMI calculator using if else

---
+++ challenges/01-Fundamentals-Part-1/challenge002-bmi-with-if-else/script.js
@@ -0,0 +1,30 @@
+/*
+CHALLENGE #2
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, telling the user who has the higher BMI. The message can be either:
+
+"Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!".
+
+2. Modify the outputs above to use template literals to include the BMI values in the outputs.
+
+Example: "Mark's BMI (28.3) is higher than John's (23.9)!" or "John's BMI (29.1) is higher than Mark's (27)!".
+
+Note: Don't round the BMI values. Leave them as they are.
+*/
+const massMark = 75;
+const heightMark = 1.89;
+const massJohn = 60;
+const heightJohn = 1.90;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+/* Write your code below. Good luck! 🙂 */
+
+if(BMIMark > BMIJohn) {
+    console.log("Mark's BMI is higher than John's!")
+} else {
+    console.log("John's BMI is higher than Mark's!")
+}
