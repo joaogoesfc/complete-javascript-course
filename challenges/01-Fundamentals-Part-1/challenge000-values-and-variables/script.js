bc0f5adc5b60d2ac1e425e17df5fe35a92baa572
João Góes <goes.joaoseje@gmail.com>
2024-02-28 14:47:48 -0300

    Exercise 001 values and variables

---
+++ challenges/01-Fundamentals-Part-1/challenge000-values-and-variables/script.js
@@ -0,0 +1,18 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23)
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 26; // allowed $, _ and letters at the beginning of the variables
+
+let PI = 3.1415; // constant
+
+let myFirstJob = "Programmer"; // better than job1
