eaec6ccfd2f7ac6128d8f8332f2bcea65cfd9a64
João Góes <goes.joaoseje@gmail.com>
2024-03-01 17:15:24 -0300

    Create: ex017-arrow-functions

---
+++ exercises/02-Fundamentals-Part-2/ex017-arrow-functions/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+
+const age3 = calcAge3(2000);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years`
+}
+
+console.log(yearsUntilRetirement(2001, "João"));
+console.log(yearsUntilRetirement(1980, "Bob"));
