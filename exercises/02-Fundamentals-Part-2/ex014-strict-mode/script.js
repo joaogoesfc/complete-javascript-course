170c2f33ede8bda8f101c8fc0f3c65136402cbc2
João Góes <goes.joaoseje@gmail.com>
2024-03-01 14:20:22 -0300

    Create: ex014-strict-mode

---
+++ exercises/02-Fundamentals-Part-2/ex014-strict-mode/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive");
+
+//const interface = "Audio";
+//const private = 534;
+//const if = 23;
