9e3ccc9608c3f635781ff85aa28febc8e710c77c
João Góes <goes.joaoseje@gmail.com>
2024-03-03 17:18:41 -0300

    Create: ex021-objects

---
+++ exercises/02-Fundamentals-Part-2/ex021-objects/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const joaoArray = [
+    "João",
+    "Góes",
+    2037 - 2001,
+    "Developer",
+    ["Daniel", "Murilo", "José"]
+];
+
+const joao = {
+    firstName: "João",
+    lastName: "Góes",
+    age: 2037 - 2001,
+    job: "Developer",
+    friends: ["Daniel", "Murilo", "José"]
+};
+
+console.log(joao.firstName);
