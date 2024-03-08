e280921dbcb299c7336ac27e7293265e50743ca2
João Góes <goes.joaoseje@gmail.com>
2024-03-07 22:27:18 -0300

    Create: ex027-while

---
+++ exercises/02-Fundamentals-Part-2/ex027-while/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//for(let rep =1; rep < 6; rep++) {
+ //   console.log(`Lifting weight repetition ${rep}`);
+//}
+console.log("-------")
+let rep = 1;
+while (rep <= 10) {
+    console.log(`Lifting weight repetition ${rep}`);
+    rep++
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if(dice === 6) console.log("Loop is about to end...")
+}
