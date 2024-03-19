8829395aeaaf06a061d065fa417577f53dbaf9b7
João Góes <goes.joaoseje@gmail.com>
2024-03-19 00:06:29 -0300

    Implementing high score of guess-my-number

--- exercises/guess-my-number/script.js
+++ exercises/guess-my-number/script.js
@@ -1,9 +1,9 @@
 'use strict';
 
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(randomNumber);
 let score = 20;
-addEventListener('click', function () {
+let highScore = 0;
+document.querySelector('.check').addEventListener('click', function () {
   const guess = document.querySelector('.guess').value;
   console.log();
 
@@ -11,11 +11,13 @@ addEventListener('click', function () {
   if (!guess) {
     console.log('No number!');
   } else if (guess == randomNumber) {
+    highScore = score > highScore ? score : highScore;
+
     document.querySelector('.message').textContent = 'Correct!';
-    this.document.querySelector('body').style.backgroundColor = "green";
-    this.document.querySelector('.number').style.width = "30rem";
-    randomNumber = Math.trunc(Math.random() * 20);
-    console.log(randomNumber);
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = guess;
+    document.querySelector('.highscore').textContent = highScore;
   } else if (guess < 0 || guess > 20) {
     document.querySelector('.message').textContent =
       'The number must be between 0 and 20';
@@ -30,8 +32,21 @@ addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     }
   } else {
-    this.document.querySelector('.message').textContent = 'You loose the game!';
+    document.querySelector('.message').textContent = 'You loose the game!';
     score--;
     document.querySelector('.score').textContent = score;
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  score = 20;
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('.guess').value = '';
 });
